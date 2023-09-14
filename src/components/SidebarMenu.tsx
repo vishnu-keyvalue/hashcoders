ccefbc4fba47893074dc696167d01e9106000fd3
Vishnu M Menon <vishnu.m@keyvalue.systems>
2023-09-14 12:08:16 +0530

    Create Sidebar component

---
+++ src/components/SidebarMenu.tsx
@@ -0,0 +1,31 @@
+import { FC } from 'react';
+import { Sidebar, Menu, MenuItem } from 'react-pro-sidebar';
+import Icon from './Icon';
+import Link from 'next/link';
+
+const SidebarMenu: FC = () => {
+    const menuItemArray = [
+        {name: 'Dashboard', icon: '/icons/dashboard.png', page: '/username', width: 30, height: 30},
+        {name: 'Create Listing', icon: '/icons/create-listing.png', page: '/username'},
+        {name: 'View Listing', icon: '/icons/view-listing.png', page: '/username'},
+        {name: 'Analysis', icon: '/icons/analysis.png', page: '/username'},
+        {name: 'Logout', icon: '/icons/logout.png', page: '/username', width: 30, height: 30}
+    ];
+    return (
+        <Sidebar rootStyles={{minHeight: '89vh'}}>
+            <h4 style={{margin: 20}}>Ad Deck</h4>
+            <Menu>
+                {menuItemArray?.map(((item, index) => (
+                    <MenuItem 
+                        key={index}
+                        icon={(<Icon url={item?.icon} width={item?.width} height={item?.height} />)}
+                    >
+                        <Link href={item?.page} style={{ textDecoration: 'none', color: 'black' }}>{item?.name}</Link>
+                    </MenuItem>
+                )))}
+            </Menu>
+        </Sidebar>
+    );
+};
+
+export default SidebarMenu;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import Container from "react-bootstrap/Container";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import TextInput from "@/components/TextInput";
+import SidebarMenu from "@/components/SidebarMenu";
 
 export default function Home() {
   return (
@@ -13,11 +14,14 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Container className="d-flex" style={{marginLeft: 0, paddingLeft: 0}}>
+      <SidebarMenu />
       <Container as="main" className="py-4 px-3 mx-auto">
         <Header />
         <h1 className="display-6">Keycode 2023 Submission</h1>
         <TextInput />
         <Footer />
+      </Container>
       </Container>
     </>
   );
