6754105943b56f6aaf83d6a39d7c170daebabb5e
Vishnu M Menon <vishnu.m@keyvalue.systems>
2023-09-14 21:11:51 +0530

    Create Listing Page setup

--- src/components/SidebarMenu.tsx
+++ src/components/SidebarMenu.tsx
@@ -1,16 +1,17 @@
 import { FC } from 'react';
 import { Sidebar, Menu, MenuItem } from 'react-pro-sidebar';
 import Icon from './Icon';
-import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 const SidebarMenu: FC = () => {
     const menuItemArray = [
-        {name: 'Dashboard', icon: '/icons/dashboard.png', page: '/username', width: 30, height: 30},
-        {name: 'Create Listing', icon: '/icons/create-listing.png', page: '/username'},
-        {name: 'View Listing', icon: '/icons/view-listing.png', page: '/username'},
-        {name: 'Analysis', icon: '/icons/analysis.png', page: '/username'},
-        {name: 'Logout', icon: '/icons/logout.png', page: '/username', width: 30, height: 30}
+        {name: 'Dashboard', icon: '/icons/dashboard.png', page: '/host/create-listing', width: 30, height: 30},
+        {name: 'Create Listing', icon: '/icons/host/create-listing.png', page: '/host/create-listing'},
+        {name: 'View Listing', icon: '/icons/view-listing.png', page: '/host/create-listing'},
+        {name: 'Analysis', icon: '/icons/analysis.png', page: '/host/create-listing'},
+        {name: 'Logout', icon: '/icons/logout.png', page: '/host/create-listing', width: 30, height: 30}
     ];
+    const { push } = useRouter();
     return (
         <Sidebar rootStyles={{minHeight: '89vh', position: 'fixed', bottom: 0, top: 0,
         boxShadow: '0px 0px 32px 0px rgba(136, 152, 170, 0.15)'}}>
@@ -19,9 +20,10 @@ const SidebarMenu: FC = () => {
                 {menuItemArray?.map(((item, index) => (
                     <MenuItem 
                         key={index}
+                        onClick={() => push(item?.page)}
                         icon={(<Icon url={item?.icon} width={item?.width} height={item?.height} />)}
                     >
-                        <Link href={item?.page} style={{ textDecoration: 'none', color: 'black' }}>{item?.name}</Link>
+                        {item?.name}
                     </MenuItem>
                 )))}
             </Menu>
--- src/pages/host/create-listing.tsx
+++ src/pages/host/create-listing.tsx
@@ -1,9 +1,13 @@
 import PrivateLayout from "@/components/PrivateLayout";
+import { Container } from "react-bootstrap";
 
 const CreateListingPage = () => {
   return (
     <>
-      <PrivateLayout title="CREATE LISTING" />
+      <PrivateLayout title="CREATE LISTING">
+        <Container className="w-25 h-25">
+        </Container>
+      </PrivateLayout>
     </>
   );
 };
